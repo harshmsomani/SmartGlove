3385fd02f781eceb7081513cb5e1e10d1cf39db5
harshmsomani <harshmsomani@outlook.com>
2021-08-17 14:31:48 +0530

    Adding comments and fixed sgParticipants error to include .git for setdiff

--- sgLoadData.m
+++ sgLoadData.m
@@ -1,22 +1,33 @@
 function data = sgLoadData(myDirectory)
-%sgLoadData Summary of this function goes here
-%   Detailed explanation goes here
+%sgLoadData loads required Participant data for pre-processing tasks
+%   The input variable is the working directory with glove data for all
+%   participants. The output is a timetable consisting of a observation
+%   timestamp, glove name, activity number, pitch and roll values. 
+
+%Prompt user to enter participant number 
 prompt = 'Enter Participant Number: ';
 pNum = input(prompt);
 
-sgParticipants = setdiff({myDirectory([myDirectory.isdir]).name}, {'.','..'});
+%Use directory and set operations to obtain name of subfolders
+sgParticipants = setdiff({myDirectory([myDirectory.isdir]).name}, {'.','..','.git'});
+%Create a tabular text datastore of all raw data for selected participant
 ds = tabularTextDatastore(sgParticipants{pNum},'IncludeSubfolders',true,'FileExtensions','.csv');
 
+%Filter columns required for further analysis and reassing to datastore
 idx = ismember(ds.VariableNames,["name","time","activity","pitch","roll"]);
 ds.SelectedVariableNames = ds.VariableNames(idx);
 
+%Convert formatting of name column in datastore to type categorical
 dsFormats = string(ds.SelectedFormats);
 dsFormats(1) = "%C";  
 ds.SelectedFormats = dsFormats;
 
+%Load all files in datastore
 data = readall(ds);
+%Convert time stamp to readable data time format
 data.time = datetime(uint64(data.time)/1e9, 'ConvertFrom', 'posixtime', 'Format', 'dd-MMM-yyyy HH:mm:ss');
 
+%Remove activity '-1' from data and convert to timetable
 data(ismember(data.activity,-1),:)=[];
 data = table2timetable(data);
 end
